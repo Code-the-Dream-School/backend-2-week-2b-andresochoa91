abee533afa58fe9f8f4ce8993d3a0998ea7f6506
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-30 22:51:57 -0700

    Some updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+subscribers = Subscriber.create([
+  {name: 'Jay'},
+  {name: 'Pasan'}
+])
+
+magazines = Magazine.create([
+  {title: 'Ruby Reader'},
+  {title: 'iOS Inspired'}
+])
+
